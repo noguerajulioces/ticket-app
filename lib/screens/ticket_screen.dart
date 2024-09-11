b56e8cffd064a5d6b851817f57d2372263040ae5
noguerajulioces <noguerajulio24@gmail.com>
2024-09-11 16:33:20 -0400

    First implementation of tts

--- lib/screens/ticket_screen.dart
+++ lib/screens/ticket_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-class TicketScreen extends StatelessWidget {
-  const TicketScreen({super.key});
+class TicketScreen extends StatefulWidget {
+  @override
+  _TicketScreenState createState() => _TicketScreenState();
+}
+
+class _TicketScreenState extends State<TicketScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();
+
+    flutterTts.setLanguage("es-MX");
+    flutterTts.setPitch(0.7);
+    flutterTts.setSpeechRate(1);
+  }
+
+  // Método para reproducir la voz del ticket
+  Future<void> _speakTicketNumber() async {
+    await flutterTts.speak("Ticket J6");
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-
     double ticketFontSize = screenWidth * 0.1;
 
     return Scaffold(
@@ -21,20 +47,23 @@ class TicketScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text(
-                      'Ticket #',
+                      'Ticket',
                       style: TextStyle(
-                        fontSize: 36,
+                        fontSize: 24,
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     const SizedBox(height: 10),
-                    Text(
-                      'J6',
-                      style: TextStyle(
-                        fontSize: ticketFontSize,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                    GestureDetector(
+                      onTap: _speakTicketNumber,
+                      child: Text(
+                        '#J6',
+                        style: TextStyle(
+                          fontSize: ticketFontSize,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ],
